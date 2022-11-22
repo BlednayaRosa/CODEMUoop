5eddb14cb6d9fb621aab8c9a3d82942c3b9e29f4
BlednayaRosa <103933583+BlednayaRosa@users.noreply.github.com>
2022-11-22 11:07:24 +0300

    Add files via upload

---
+++ 22.11.2022/21.1.js
@@ -0,0 +1,14 @@
+
+// 1 -------------------------------
+        	/*true
+			  false*/
+class Student {
+	
+}
+class Employee {
+	
+}
+
+let user = new Employee;
+console.log(user instanceof Employee);
+console.log(user instanceof Student);
